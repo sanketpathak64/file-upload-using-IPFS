201efbd083149ecdf0d0620949a91423efef7285
sanket pathak <sanketpathak64@gmail.com>
2020-03-26 10:11:48 +0530

    changed from image to file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # file-upload-using-IPFS
 starter kit is taken from https://github.com/dappuniversity/starter_kit
 It a basic repo demonstrating how a file is uploaded by IPFS on blockchain
+how to run
+1.truffle migrate
+2.truffle deploy
+3.npm start
--- src/components/App.js
+++ src/components/App.js
@@ -106,7 +106,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -123,15 +123,11 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
+                
+                  <a href={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`}>
+                    <button>Download me</button>
+                    </a>
                   
-
-                </a>
                 <br />
                 <h1>Meme of the day</h1>
                 <form onSubmit={this.onSubmit}>
